e8c0ecbce8022e14988a91705f4bc887ec5c4ed1
20161104585 <Hl5618270967>
2018-06-25 10:48:58 +0800

    Initial Commit

---
+++ point text/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  point text
+//
+//  Created by 20161104585 on 2018/6/25.
+//  Copyright © 2018年 20161104585. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
